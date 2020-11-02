a9b7f90a1541e9bdaf75e8c5fb5a656c77d487b0
Ted Balmer <54968949+midkiffaries@users.noreply.github.com>
2020-11-01 22:09:42 -0600

    11-1-2020

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,7 +5,7 @@
 "use strict";    
 
 // REMLAB version
-const RemlabVersion = "4.9.2010 DEV";
+const RemlabVersion = "4.9.2011 DEV";
 const TodaysDate = new Date();
 
 // Declare Global Variables and settings
